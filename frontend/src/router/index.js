f1ba9e49fb82a63fe7eea2fa8a46130a67800d70
Zong <love970120@gmail.com>
2024-07-13 12:55:20 +0800

    feat: Add plan router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,12 +4,14 @@ import React from "react";
 import Home from "../pages/HomePage/HomePage";
 import Login from "../pages/login/Login";
 import Explore from "../pages/explore/Explore";
+import Planning from "../pages/Planning/Planning";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
+  { path: "/plan", element: <Planning /> },
   {
     path: "/explore",
     element: <Explore />,
